4e2169fc5ddcd955df3b9913242ef4276a09f398
Bee <121594751+Just-a-Bee@users.noreply.github.com>
2025-01-12 15:35:38 +0000

    First commit, main scene and drone movement created

---
+++ drone.gd
@@ -0,0 +1,67 @@
+extends CharacterBody3D
+
+#movement vars
+var horizontal_acc = 3
+var vertical_acc = 2
+var turn_acc = 1
+
+var horizontal_max = 5
+var vertical_max = 5
+var turn_max = 1
+
+var turn_velocity = 0
+
+var gravity = 1
+var air_resistance = .4
+var turn_slowing = .1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _physics_process(delta):
+	# Do player movement
+	var move_axis = get_move_axis()
+	
+	# do turning
+	turn_velocity = move_toward(turn_velocity, move_axis.x, turn_acc * delta)
+	rotation.y += turn_velocity # rotate left/right
+	
+	# do forward/back
+	var forward_dir = Vector3.FORWARD.rotated(up_direction, rotation.y)
+	velocity += forward_dir * move_axis.z * horizontal_acc * delta # add forward/back
+	var velocity2D = Vector3(velocity.x, 0, velocity.z)
+	if velocity2D.length() > horizontal_max:
+		velocity2D = velocity2D.normalized() * horizontal_max # limit to horizontal_max
+	
+	# Add up movement
+	velocity.y = move_toward(velocity.y, vertical_max, move_axis.y * vertical_acc * delta)
+	
+	# Other forces
+	velocity -= Vector3.UP * gravity * delta # Do gravity
+	velocity -= velocity2D.normalized() * air_resistance * delta
+	turn_velocity = move_toward(turn_velocity, 0, turn_slowing)
+	
+	move_and_slide()
+
+func get_move_axis()->Vector3:
+	var move_axis:Vector3 = Vector3.ZERO
+	
+	if Input.is_action_pressed("drone_forward"):
+		move_axis.z += 1
+	if Input.is_action_pressed("drone_back"):
+		move_axis.z -= 1
+	if Input.is_action_pressed("drone_left"):
+		move_axis.x += 1
+	if Input.is_action_pressed("drone_right"):
+		move_axis.x -= 1
+	if Input.is_action_pressed("drone_up"):
+		move_axis.y += 1
+	
+	return move_axis
