838112928307f620c4fe11ef580ef4a6b8adad41
Bee <121594751+Just-a-Bee@users.noreply.github.com>
2025-01-15 01:42:22 +0000

    fixed bigfoot audio, drone listens now

--- UI Scenes/bigfoot_audio.gd
+++ UI Scenes/bigfoot_audio.gd
@@ -4,15 +4,15 @@ extends AudioStreamPlayer3D
 @export var player:Node
 @export var drone:Node
 
-func _process(delta):
+func _process(_delta):
 	var listener = player
 	if drone.active:
 		var player_dist = bigfoot.global_position - player.global_position
-		var drone_dist = bigfoot.global_position - player.global_position
+		var drone_dist = bigfoot.global_position - drone.global_position
 		if drone_dist.length() < player_dist.length():
 			listener = drone
+			print("drone listening")
 	
 	position = bigfoot.global_position - listener.global_position
-	print(bigfoot.global_position - position)
 	#print(position)
 	
